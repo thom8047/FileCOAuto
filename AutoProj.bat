8b1d913a2927d906fdf4a9e368d0fde0d1ba100d
thom8047 <thom8047@bears.unco.edu>
2021-08-15 15:12:54 -0600

    Updated licensing and logo in title

--- AutoProj.bat
+++ AutoProj.bat
@@ -1,5 +1,33 @@
 @echo off
 
+::license
+              
+echo     [96m _____ ---------------------------------------------
+echo     ^|   __^|---------------------------------------------
+echo     ^|  ^|_   _________ ----------------------------------
+echo     ^|   _^| ^|___   ___^|----------------------------------
+echo     ^|  ^|__     ^| ^|--------------------------------------
+echo     ^|_____^|    ^| ^|--------------------------------------
+echo                ^| ^|--------------------------------------
+echo                ^|_^|-------------------------------------- [0m
+echo     Copyright 2021-08-15 Edward Kyle Thomas Jr.
+echo     This program is free software: you can redistribute it and/or modify
+echo     it under the terms of the GNU General Public License as published by
+echo     the Free Software Foundation, either version 3 of the License, or
+echo     (at your option) any later version.
+echo                                                                   -
+echo     This program is distributed in the hope that it will be useful,
+echo     but WITHOUT ANY WARRANTY; without even the implied warranty of
+echo     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+echo     GNU General Public License for more details.
+echo                                                                   -
+echo     You should have received a copy of the GNU General Public License
+echo     along with this program.  If not, see ^<https://www.gnu.org/licenses/^>.
+echo                                                                   -
+echo     Along with the above resourses, one can find original GNU Licensing
+echo     at the original GitHub repository ^<https://github.com/thom8047/FileCOAuto/^>. 
+echo     [91m------------------------------------------------------------------------[0m 
+
 goto :after_color_comment
     [90mWhite[0m
     [91mRed[0m
